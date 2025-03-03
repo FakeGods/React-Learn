dafca20bc5886d93315233ea4e0490a99d49f0c8
FakeGods <w.leszkowicz99@gmail.com>
2025-03-03 23:15:03 +0100

    testing divs

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,23 @@
 import "./App.css";
+const Card = () => {
+  return (
+    <div>
+      <h2>
+        Card <Component></Component>
+      </h2>
+    </div>
+  );
+};
 
-function App() {}
+const App = () => {
+  return (
+    <>
+      <h2>Functional Arrow Component</h2>;
+      <Card />
+      <Card />
+      <Card />
+    </>
+  );
+};
+
 export default App;
