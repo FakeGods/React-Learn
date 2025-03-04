357334e9cd380085b2992c85779973d7815e0cf9
FakeGods <w.leszkowicz99@gmail.com>
2025-03-04 23:23:10 +0100

    adding titles? prompt?

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import "./App.css";
+const Card = () => {
+  return (
+    <div>
+      <h2>Card Component</h2>
+    </div>
+  );
+};
 
 const App = () => {
   return (
     <>
       <h2>Functional Arrow Component</h2>
+      <Card title="Star Wars" />
+      <Card title="Avatar" />
+      <Card />
     </>
   );
 };
