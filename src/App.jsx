b4805ee3df545489f19ececb5d2f632e442610b5
FakeGods <w.leszkowicz99@gmail.com>
2025-03-05 23:05:58 +0100

    props

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,32 @@ import "./App.css";
 const Card = ({ title, rating }) => {
   return (
     <div>
-      <h2>{(title, rating)}</h2>
+      <h2>
+        {title} {rating}
+      </h2>
     </div>
   );
 };
 
 const App = () => {
   return (
-    <>
+    <div>
       <h2>Functional Arrow Component</h2>
+
       <Card
         title="Star Wars"
         rating={5}
         isCool={true}
         actors={[{ name: "Actors" }]}
       />
-      <Card title="The Lord of the Rings" />
+      <Card
+        title="The Lord of the Rings"
+        rating={5}
+        isCool={true}
+        actors={[{ name: "Actors" }]}
+      />
       <Card title="Ghost in the Shell" />
-    </>
+    </div>
   );
 };
 
